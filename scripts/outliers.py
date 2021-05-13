e547c7154c9436e667ec06cf2620089c6a701ae5
ranjith1237 <ranjithreddy1061995@gmail.com>
2021-05-13 03:45:52 -0400

    education level preprocessing

--- scripts/outliers.py
+++ scripts/outliers.py
@@ -30,6 +30,20 @@ def salary_mismatch_fix(new_df):
     new_df.drop(index_names, inplace = True)
     return new_df
 
+def education_Level(new_df):
+    new_df = new_df[new_df['EdLevel'].notna()]
+    #new_df["Edlevel"][(new_df["CompTotal"]>salaries[i]) & (new_df["CompFreq"]=="Weekly") & (new_df['Country']==countries[i])] = new_df["CompTotal"]
+    new_df["EdLevel"].loc[new_df['EdLevel'].str.contains(r'Bachelor(?!$)')]="Bachelor's degree"
+    new_df["EdLevel"].loc[new_df['EdLevel'].str.contains(r'Master(?!$)')]="Master's degree"
+    new_df["EdLevel"].loc[new_df['EdLevel'].str.contains(r'doctoral(?!$)')]="Doctoral degree"
+    new_df["EdLevel"].loc[new_df['EdLevel'].str.contains(r'never(?!$)')]="Other"
+    new_df["EdLevel"].loc[new_df['EdLevel'].str.contains(r'Professional(?!$)')]="Other"
+    new_df["EdLevel"].loc[new_df['EdLevel'].str.contains(r'Associate(?!$)')]="Associate degree"
+    new_df["EdLevel"].loc[new_df['EdLevel'].str.contains(r'Secondary(?!$)')]="Secondary school"
+    new_df["EdLevel"].loc[new_df['EdLevel'].str.contains(r'Some(?!$)')]="Secondary school"
+    return new_df
+
+
 if __name__ == "__main__":
     path="../Data/merged_data.csv"
     df=pd.read_csv(path)
@@ -37,4 +51,5 @@ if __name__ == "__main__":
     new_df=year_filter(df)
     new_df=workweekhrs(new_df)
     new_df=salary_mismatch_fix(new_df)
+    new_df=education_Level(new_df)
     new_df.to_csv("./../Data/merged_data_1.csv",na_rep='N/A')
